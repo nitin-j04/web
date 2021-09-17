77361384efc2f0bc8335cb798e29f4b77133c233
nitin-j04 <nitinpilania5@gmail.com>
2021-09-17 16:58:51 +0530

    creatign db on atlas

--- src/conn.js
+++ src/conn.js
@@ -1,12 +1,12 @@
 const mongoose=require("mongoose");
 
-
+const db = process.env.DB || "mongodb://localhost:27017/foodorder";
   //connection creation
-mongoose.connect("mongodb://localhost:27017/foodorder", { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true },(err,database) => {
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true },(err,database) => {
     if(err) {
       return console.log(err);
     }
-    db = database;
+  
     console.log("database connected successfully...");
   });
  
